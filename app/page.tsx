9f9bf23b34967022d2df6eb8a462e21b6374dd4f
Vikash Thakur <thakvikash622@gmail.com>
2024-10-15 23:54:20 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <Navbar />
       <HeroPage />
       <FeaturePage />
+      <HeroPage />
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          'pbs.twimage.com',  // Add this domain
+          'upload.wikimedia.org',  // Example of another domain
+          'example.com', // You can add more domains here
+        ],
+      },
+};
 
 export default nextConfig;

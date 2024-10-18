fcbbbd9d3954647e3802159f906639486afb4104
Vikash Thakur <thakvikash622@gmail.com>
2024-10-18 17:23:55 +0530

    Updated the code for Small and large device

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-// import DeskNavbar from './DeskNavbar'
-import MobNavbar from './MobNavbar'
+'use client'
+import React, { useState, useEffect } from 'react';
+import DeskNavbar from './DeskNavbar';
+import MobNavbar from './MobNavbar';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to check screen size
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768); // You can adjust the width threshold as needed
+  };
+
+  useEffect(() => {
+    // Check the screen size on initial render
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div>
-      {/* <DeskNavbar /> */}
-      <MobNavbar />
+      {isMobile ? <MobNavbar /> : <DeskNavbar />}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
